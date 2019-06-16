657d858d94a692cb20b8dfedd6eccc488660a4ae
Jeremy Anderson <jaa@Jeremys-MacBook-Pro.local>
2019-06-16 06:15:38 -0700

    Initial commit

---
+++ automate.py
@@ -0,0 +1,175 @@
+import pandas as pd
+from selenium import webdriver
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from difflib import get_close_matches, SequenceMatcher
+from time import sleep
+import uo_config
+
+# Change index numbers here
+# COULD GO IN A CONFIG FILE
+index_nums = uo_config.index_nums
+
+
+# Access Purchasing Google Sheet
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name(uo_config.uoauto_sheet_keyfile, scope)
+client = gspread.authorize(creds)
+# Get sheet data
+sheet = client.open('Purchasing Requests ').sheet1
+data = sheet.get_all_records()
+df = pd.DataFrame(data)
+# Get new orders
+# CHANGE THIS, INSTEAD OF A COPY WORK WITH THE ORIGINAL DATAFRAME SO THE
+# GOOGLE SHEET CAN BE UPDATED AFTER
+new_orders = df[df['Order Placed'].apply(lambda x: x.lower()) != 'x'].copy()
+new_orders = new_orders.iloc[get_consecutive_index(new_orders),:]
+# Fix index numbers
+new_orders.loc[:, "Index # used"] = check_index_nums(new_orders["Index # used"])
+# Split orders by index # and vendor name
+individual_orders = group_orders(new_orders)
+
+
+# Connect to the ordering website
+site = uo_config.site
+
+driver = webdriver.Chrome(uo_config.chrome_driver)
+driver.get(site)
+
+for order in individual_orders:
+    # Fill in the head info
+    fill_form_header(driver)
+    # Fill in the vendor info for the order
+    fill_form_vendor_index(driver, order)
+    if len(order) < 6:
+        # Fill in the item info
+        fill_form_item(driver, order)
+        submit_button = driver.find_element_by_name("Submit") 
+        submit_button.click()
+    else:
+        print('What! This never happens!')
+        break
+    driver.get(site)
+
+
+def fill_form_item(driver, order):
+    quantities = driver.find_elements_by_name("quantity[]")
+    sizes = driver.find_elements_by_name("size[]")
+    catnums = driver.find_elements_by_name("catnum[]")
+    descriptions = driver.find_elements_by_name("description[]")
+    prices = driver.find_elements_by_name("unit_price[]")
+
+    for n, index in enumerate(order.index):
+        quantities[n].send_keys(order.loc[index, "Quantity"])
+        sizes[n].send_keys(order.loc[index, "Size"])
+        catnums[n].send_keys(order.loc[index, "Catalog #"])
+        descriptions[n].send_keys(order.loc[index, "Description"])
+        prices[n].send_keys(order.loc[index, "Unit Price"][1:])
+
+
+def fill_form_vendor_index(driver, order):
+    vendor = driver.find_element_by_name("vendor")
+    vendor.send_keys(order["Supplier"].values[0])
+    index = driver.find_element_by_name("index")
+    index.send_keys(order["Index # used"].values[0])
+
+
+def fill_form_header(driver):
+    """Fill the usual information that is in the form.
+    """
+
+    lab = driver.find_element_by_name("lab")
+    lab.send_keys(uo_config.info["lab"])
+    requested_by = driver.find_element_by_name("requestor")
+    requested_by.send_keys(uo_config.info["requestor"])
+    email = driver.find_element_by_name("email")
+    email.send_keys(uo_config.info["email"])
+    activity_code = driver.find_element_by_name("activity_code")
+    activity_code.send_keys(uo_config.info["activity_code"])
+
+
+def get_consecutive_index(df):
+    """Given a dataframe find rows that have consecutive indices
+
+    Parameters
+    ----------
+    df  :   pandas.DataFrame object
+
+    Returns
+    -------
+    list of indices that are consecutive
+    """
+
+    indices = list()
+    for n in range(len(df)):
+        i = df.index[n]
+        if n+1 < len(df):
+            i1 = df.index[n+1]
+            indices.append(i1 - i)
+        else:
+            indices.append(1)
+    
+    return [n for n, i in enumerate(indices) if i==1]
+
+
+def check_index_nums(col, index_nums=index_nums, autofill=index_nums['NIH']):
+    """Check the ordering index numbers of the orders. If there is none put use the
+    NIH index.
+
+    Parameters
+    ----------
+    df      :   pandas.DataFrame object
+    autofill    :   str
+        Grant index number to fill in for missing values 
+    """
+
+    indexes = list()
+    for i in col:
+        if i == '':
+            indexes.append(autofill)
+        else:
+            match = get_close_matches(i, index_nums.values(), n=1, cutoff=.6)
+            if len(match) == 0:
+                indexes.append(autofill)
+            else:
+                indexes.append(match[0])
+
+    return indexes
+
+
+def group_orders(df):
+    """
+    """
+
+    orders = list()
+    for _, x in df.groupby(df["Index # used"]): 
+         for _, y in x.groupby(x["Supplier"]): 
+             orders.append(y) 
+
+    return orders
+
+
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).quick_ratio()
+
+
+def consolidate_vendors(df):
+    """
+    Dormant for now. Might muck around with later.
+    """
+
+    suppliers = df["Supplier"].unique()
+    for i, supplier_1 in enumerate(df["Supplier"].items()):
+        for supplier_2 in suppliers:
+            score = similar(supplier_1, supplier_2)
+            if supplier_1 == supplier_2:
+                print('same')
+                continue
+            elif score >= 0.9:
+                print('change: {} vs. {}'.format(supplier_2, supplier_1))
+                df.loc[i, "Supplier"] = supplier_2
+                # Update the unique list???
+                suppliers = df["Supplier"].unique()
+            else:
+                print('keep: {} vs. {}'.format(supplier_2, supplier_1))
